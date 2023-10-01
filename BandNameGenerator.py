1b523ee6832198e2b3fcdf54e9728188cb371a4d
Erin O'Connor <146624514+erinmoconnor1@users.noreply.github.com>
2023-10-01 20:19:15 +0900

    Add files via upload

---
+++ BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!")
+city_name = input("What city did you grow up in?\n")
+pet_name =  input("What is the name of your pet?\n")
+print("Your new band name is " + city_name + " " + pet_name + "!")
